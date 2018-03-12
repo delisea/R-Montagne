dd9aa63422e120abdf03561b85ab152e6c606643
Hugo-AF <amodrufavin.hugo@gmail.com>
2018-03-12 11:25:04 +0100

    Login correction

--- MobileApp/src/providers/auth-service/auth-service.ts
+++ MobileApp/src/providers/auth-service/auth-service.ts
@@ -97,8 +97,8 @@ export class AuthService {
             this.scheduleSingleNotif(alert);
           }
         });
-
-        this.events.publish('log:change', this.logres.success === 1);
+        if(this.logres.success === 1)
+          this.events.publish('log:change', this.logres.success === 1);
         observer.complete();
       });
     });
